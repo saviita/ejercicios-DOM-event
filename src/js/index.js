82eee7e2b892c2b115ba2b44ab72f32f7e34a51e
saviita <taatt2000@gmail.com>
2024-06-24 11:30:16 +0200

    24/06

--- src/js/index.js
+++ src/js/index.js
@@ -13,19 +13,21 @@ const titleElement = document.getElementById('title');
 const titleContent = titleElement.textContent;
 
 const addText = () => {
+  //even.target.textContent = 'Quita de encima!!';
   titleElement.textContent = 'Quita de encima!!';
 };
-titleElement.addEventListener('mouseover', addText);
 
 const changeText = () => {
   titleElement.textContent = titleContent;
 };
 
+titleElement.addEventListener('mouseover', addText);
 titleElement.addEventListener('mouseout', changeText);
 
 //EJ3 Agrega un evento de cambio de tamaño de ventana y te muestre por consola el tamaño de la ventana.
 const sizeWindow = () => {
   console.log(`${window.visualViewport.height} x ${window.visualViewport.width}`);
+  //even.target.innerWidth
 };
 window.addEventListener('resize', sizeWindow);
 
@@ -35,7 +37,12 @@ const textElement = document.getElementById('text');
 const printKey = event => {
   textElement.textContent = event.key;
 };
+
+const waitingInfo = event => {
+  textElement.textContent = 'Esperando entrada de teclado';
+};
 window.addEventListener('keypress', printKey);
+window.addEventListener('keyup', waitingInfo);
 
 //EJ5 Crea un array con 5 palabras. Añade un h2 a tu HTML y ponle un id. Añade dos botones con el texto previus y next respectívamente. Haz que los botones cambien el texto del h2 con las palabras del array, cuando llegues a la última volverás a la primera si pulsas next y cuando estés en la primera podrás volver a la última dandole a previous.
 const smallTitleElement = document.getElementById('title-small');
@@ -47,7 +54,7 @@ let number = 0;
 
 smallTitleElement.textContent = fiveWords[0];
 
-const next = () => {
+/* const next = () => {
   console.log(number);
   if (number >= fiveWords.length - 1) {
     //number = 0;
@@ -59,14 +66,40 @@ const next = () => {
   smallTitleElement.textContent = fiveWords[number];
 };
 
+//CONDICIONAL CON TERNARIO number >= fiveWords.length ? (number = 0) : number++;
+
 const previous = () => {
   console.log(number);
   if (number <= 0) {
-    number = fiveWords.length - 1;
-  } else {
-    previousElement.disabled = true;
+    //number = fiveWords.length - 1;
     nextElement.disabled = false;
-    //number--;
+    previousElement.disabled = true;
+  } else {
+    number--;
+  }
+  smallTitleElement.textContent = fiveWords[number];
+}; */
+
+//EJ6 Repite el mismo ejercicio pero esta vez desactiva el botón previous si estás en el primer elemento y el botón next si estás en el último
+
+const next = () => {
+  previousElement.disabled = false;
+  number++;
+
+  if (number === fiveWords.length - 1) {
+    nextElement.disabled = true;
+  }
+  smallTitleElement.textContent = fiveWords[number];
+};
+
+//Es importante primero restar o sumar y luego hacer la comprobación
+
+const previous = () => {
+  nextElement.disabled = false;
+  number--;
+
+  if (number === 0) {
+    previousElement.disabled = true;
   }
   smallTitleElement.textContent = fiveWords[number];
 };
@@ -74,14 +107,11 @@ const previous = () => {
 nextElement.addEventListener('click', next);
 previousElement.addEventListener('click', previous);
 
-//EJ6 Repite el mismo ejercicio pero esta vez desactiva el botón previous si estás en el primer elemento y el botón next si estás en el último
-
-//return
-
 //EJ7 Crea un input range con un label, al mover el input range deberá escribir en el label el valor del input range.
 const label = document.getElementById('label');
 const range = document.getElementById('range');
 
+label.textContent = range.value;
 const printValue = () => {
   label.textContent = range.value;
 };
@@ -93,7 +123,9 @@ const resultElement = document.getElementById('result');
 const textResultElement = document.getElementById('text-result');
 
 const printCheck = () => {
+  textResultElement.textContent = '';
   const checkNumberElement = document.querySelectorAll('input:checked');
+  //input[type=checkbox]
   resultElement.textContent = checkNumberElement.length;
 
   checkNumberElement.forEach(check => {
